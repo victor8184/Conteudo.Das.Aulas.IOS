2f65e1114ea7269bafd77b7a4096e9c3b407f371
Victor <victinhohugofigueiredo@gmail.com>
2023-11-23 21:26:08 -0300

    tabela incompleta

---
+++ react/aulas04pt2/agenda_tema_05/src/Components/Tasks.jsx
@@ -0,0 +1,32 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Consulta médica',
+        day: '5 de Fev as 14:30',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Reunião na Escola',
+        day: '6 de Fev as 13:30',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Compras no Supermercado',
+        day: '7 de Fev as 8:30',
+        reminder: false,
+    },
+];
+
+const Tasks = () => {
+    return (
+        <>
+            {tasks.map((task) => (
+                <h3>{task.text}</h3>
+            ))}
+        </>
+    );
+};
+
+export default Tasks;
